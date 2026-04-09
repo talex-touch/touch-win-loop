36c0f18cc8510e23cb266cde037a88a3b93b088f
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2026-04-10 02:54:47 +0800

    fix(ci): resolve unsafe fetch typing

--- app/plugins/unsafe-fetch.ts
+++ app/plugins/unsafe-fetch.ts
@@ -1,11 +1,7 @@
 type UnsafeFetch = <T = any>(request: string, options?: any) => Promise<T>
 
-declare global {
-  var unsafeFetch: UnsafeFetch
-}
-
 export default defineNuxtPlugin(() => {
-  globalThis.unsafeFetch = ((request, options = {}) => {
+  ;(globalThis as typeof globalThis & { unsafeFetch: UnsafeFetch }).unsafeFetch = ((request, options = {}) => {
     return $fetch(request, {
       ...options,
       credentials: options?.credentials ?? 'include',
